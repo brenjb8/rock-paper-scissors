75f1f0ce7a6da72a60c4df1711e68c8cdb15b25b
brendanjb8 <brendanb321@gmail.com>
2021-02-22 15:43:39 -0600

    playRound function complete

--- js/script.js
+++ js/script.js
@@ -1,12 +1,36 @@
 //Make computer select either Rock, Paper, or Scissors
 function computerPlay() {
     let moves = ["Rock", "Paper", "Scissors"];
-    //let randomMove = moves[Math.floor(Math.random() * moves.length)];
     return moves[Math.floor(Math.random() * moves.length)];
-}
 
-computerPlay();
+}
 
 //Player Selection
 
 let playerInput = prompt("Enter either Rock, Paper, or Scissors: ");
+
+
+//Play a round
+
+function playRound(playerInput, computerPlay) {
+    let computerSelection = computerPlay();
+    let playerSelection = playerInput.charAt(0).toUpperCase() + playerInput.slice(1).toLowerCase();
+
+    if (playerSelection !== "Rock" && playerSelection !== "Paper" && playerSelection !== "Scissors") {
+        return "Enter a valid play!";
+    } else {
+
+        if (playerSelection == "Paper" && computerSelection == "Rock") {
+            return "Good job, you won! Paper beats Rock!";
+        } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+            return "Good job, you won! Scissors beats Paper!";
+        } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+            return "Good job, you won! Rock beats Scissors!";
+        } else {
+            return "You lose! Better luck next time!";
+        }
+    }
+}
+
+console.log(playRound(playerInput, computerPlay));
+
